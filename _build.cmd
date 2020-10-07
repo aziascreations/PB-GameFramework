5b01e579f7ccc7abff86ee88a2fde36763923eeb
Herwin <herwin.bozet@gmail.com>
2020-10-07 22:02:59 +0200

    Overhauled build scripts and moved libraries

--- _build.cmd
+++ _build.cmd
@@ -1,47 +1,61 @@
 @echo off
+chcp 65001 >nul
+echo ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+echo ┃ Automated build script v1.0 ┃
+echo ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+set CPLR86="C:\Program Files\PureBasic\PureBasic_5.70_x86_x64\Compilers\pbcompiler.exe"
+set CPLR64="C:\Program Files\PureBasic\PureBasic_5.70_x64\Compilers\pbcompiler.exe"
+echo.
 
-:: clean, compile, build
-set PATH=%PATH%;C:\Program Files\PureBasic\PureBasic_5.70_x64\Compilers\
-
+echo ━┫ Preliminary clean-up ┣━
+echo ● Deleting Build/
 rmdir /Q /S Build\
+echo ● Deleting Packages/
 rmdir /Q /S Packages\
+echo ● Deleting executables
 del .\*.exe
+echo.
 
-:: x86
-::mkdir Build\
-::mkdir Build\x86
-::mkdir Build\x86\Data\
-::mkdir Build\x86\Licenses\
-::"C:\Program Files\PureBasic\PureBasic_5.70_x86_x64\Compilers\pbcompiler.exe" /EXE ".\Build\x86\Game.exe" /ICON ".\icon.ico" ".\Game.pb"
-::robocopy .\ .\Build\x86\ Engine3d.dll
-::robocopy .\ .\Build\x86\ LICENSE
-::xcopy Data Build\x86\Data\ /E /Y
-::xcopy Licenses Build\x86\Licenses\ /E /Y
-
-:: x64
-::mkdir Build\x64
-::mkdir Build\x64\Data\
-::mkdir Build\x64\Licenses\
-::"C:\Program Files\PureBasic\PureBasic_5.70_x64\Compilers\pbcompiler.exe" /EXE ".\Build\x64\Game.exe" /ICON ".\icon.ico" ".\Game.pb"
-::robocopy .\ .\Build\x64\ Engine3d.dll
-::robocopy .\ .\Build\x64\ LICENSE
-::xcopy Data Build\x64\Data\ /E /Y
-::xcopy Licenses Build\x64\Licenses\ /E /Y
-
-:: Default
+echo ━┫ Environment setup ┣━
 mkdir Build\
-mkdir Build\Data\
-mkdir Build\Licenses\
-pbcompiler.exe /EXE ".\Build\Game.exe" /ICON ".\icon.ico" ".\Game.pb"
-::pbcompiler.exe /EXE ".\Build\Game.exe" ".\Game.pb"
-robocopy .\ .\Build\ Engine3d.dll
-::robocopy .\ .\Build\ LICENSE
-copy .\LICENSE ".\Build\Licenses\Custom PB Engine.txt"
-xcopy Data Build\Data\ /E /Y
-xcopy Licenses Build\Licenses\ /E /Y
-rmdir /Q /S Build\Data\Trash
-rmdir /Q /S Build\Data\Graphics\Trash
-del /s /q .\Build\*.pdn
-del /s /q .\Build\*.tga
+mkdir Build\Commons\
+mkdir Build\Commons\Data\
+mkdir Build\Commons\Licenses\
+mkdir Build\x86\
+mkdir Build\x86\Data\
+mkdir Build\x86\Licenses\
+mkdir Build\x64\
+mkdir Build\x64\Data\
+mkdir Build\x64\Licenses\
+echo.
+
+echo ━┫ Copying assets ┣━
+copy .\LICENSE ".\Build\Commons\Licenses\Custom PB Engine.txt"
+robocopy .\Libraries\x64\ .\Build\x64
+robocopy .\Libraries\x86\ .\Build\x86
+xcopy Data Build\Commons\Data\ /E /Y
+xcopy Licenses Build\Commons\Licenses\ /E /Y
+rmdir /Q /S Build\Commons\Data\Trash
+rmdir /Q /S Build\Commons\Data\Graphics\Trash
+del /s /q .\Build\Commons\*.pdn
+del /s /q .\Build\Commons\*.tga
+xcopy Build\Commons\Data\ Build\x64\Data\ /E /Y
+xcopy Build\Commons\Licenses\ Build\x64\Licenses\ /E /Y
+xcopy Build\Commons\Data\ Build\x86\Data\ /E /Y
+xcopy Build\Commons\Licenses\ Build\x86\Licenses\ /E /Y
+echo.
+
+echo ━┫ Compiling x86 ┣━
+%CPLR86% /EXE ".\Build\x86\Game.exe" /ICON ".\icon.ico" ".\Game.pb"
+echo.
+
+echo ━┫ Compiling x64 ┣━
+%CPLR64% /EXE ".\Build\x64\Game.exe" /ICON ".\icon.ico" ".\Game.pb"
+echo.
+
+echo ━┫ Final clean-up ┣━
+echo ● Deleting Build/Commons/
+rmdir /Q /S Build\Commons\
+echo.
 
 pause
--- _package.cmd
+++ _package.cmd
@@ -1,16 +1,45 @@
 @echo off
-
+chcp 65001 >nul
+echo ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+echo ┃ Automated packaging script v1.0 ┃
+echo ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
 set PATH=%PATH%;C:\Program Files\7-Zip
+set APPNAME=GameTest
+echo.
 
+echo ━┫ Preliminary clean-up ┣━
 rmdir /Q /S Packages\
 del .\*.exe
+echo.
+
+echo ━┫ Environment setup ┣━
 mkdir Packages\
+echo.
 
-7z a -mx9 ./Packages/Game.zip ./Build/*
-7z a -mx9 ./Packages/Game.7z ./Build/*
+echo ━┫ Packaging application ┣━
+7z a -mx9 ./Packages/%APPNAME%-x64.zip ./Build/x64/*
+7z a -mx9 ./Packages/%APPNAME%-x86.zip ./Build/x86/*
+7z a -mx9 ./Packages/%APPNAME%-x64.7z ./Build/x64/*
+7z a -mx9 ./Packages/%APPNAME%-x86.7z ./Build/x86/*
+7z a -mx9 ./Packages/%APPNAME%-x64.tar ./Build/x64/*
+7z a -mx9 ./Packages/%APPNAME%-x64.tar.gz ./Packages/%APPNAME%-x64.tar
+7z a -mx9 ./Packages/%APPNAME%-x86.tar ./Build/x86/*
+7z a -mx9 ./Packages/%APPNAME%-x86.tar.gz ./Packages/%APPNAME%-x86.tar
+echo.
 
+echo ━┫ Packaging source code ┣━
 :: TODO: filter out the exes that may appear in the source code folders when testing out stuff.
-7z a -mx9 ./Packages/Game-sources.zip ./Data ./Engine ./Game ./Licenses ./*.cmd ./*.pb ./*.pb.cfg ./*ico ./*.dll ./LICENSE ./readme.md ./*.pbp
-7z a -mx9 ./Packages/Game-sources.7z ./Data ./Engine ./Game ./Licenses ./*.cmd ./*.pb ./*.pb.cfg ./*ico ./*.dll ./LICENSE ./readme.md ./*.pbp
+7z a -mx9 ./Packages/%APPNAME%-sources.zip ./Data ./Engine ./Game ./Licenses ./*.cmd ./*.pb ./*.pb.cfg ./*ico ./*.dll ./LICENSE ./readme.md ./*.pbp
+7z a -mx9 ./Packages/%APPNAME%-sources.7z ./Data ./Engine ./Game ./Licenses ./*.cmd ./*.pb ./*.pb.cfg ./*ico ./*.dll ./LICENSE ./readme.md ./*.pbp
+7z a -mx9 ./Packages/%APPNAME%-sources.tar ./Data ./Engine ./Game ./Licenses ./*.cmd ./*.pb ./*.pb.cfg ./*ico ./*.dll ./LICENSE ./readme.md ./*.pbp
+7z a -mx9 ./Packages/%APPNAME%-sources.tar.gz ./Packages/%APPNAME%-sources.tar
+echo.
+
+echo ━┫ Packaging repository ┣━
+7z a -mx9 ./Packages/%APPNAME%-sources+git.zip ./Data ./Engine ./Game ./Licenses ./*.cmd ./*.pb ./*.pb.cfg ./*ico ./*.dll ./LICENSE ./readme.md ./*.pbp ./.git ./.gitignore
+7z a -mx9 ./Packages/%APPNAME%-sources+git.7z ./Data ./Engine ./Game ./Licenses ./*.cmd ./*.pb ./*.pb.cfg ./*ico ./*.dll ./LICENSE ./readme.md ./*.pbp ./.git ./.gitignore
+7z a -mx9 ./Packages/%APPNAME%-sources+git.tar ./Data ./Engine ./Game ./Licenses ./*.cmd ./*.pb ./*.pb.cfg ./*ico ./*.dll ./LICENSE ./readme.md ./*.pbp ./.git ./.gitignore
+7z a -mx9 ./Packages/%APPNAME%-sources+git.tar.gz ./Packages/%APPNAME%-sources+git.tar
+echo.
 
 pause
